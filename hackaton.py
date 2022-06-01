8099b3ebad935968dde7f9891cf9cff6710e2f86
GabrielRD <grd342@outlook.com>
2022-06-01 12:40:47 -0300

    feat: python_function

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,14 @@
-# Desenvolva seu código abaixo
+from pip import main
+from pacotes import funcoes
+
+
+hackathon_1 = ["Team Kenzie", "Team Ateliware", "Team VHSYS", "Team Mirum"]
+hackathon_2 = ["Team Ateliware", "Team Kenzie", "Team VHSYS", "Team Mirum"]
+hackathon_3 = ["Team Mirum", "Team Ateliware","Team VHSYS", "Team Kenzie"]
+
+
+
+def main_func ():
+    return funcoes.get_score("Team Kenzie", hackathon_3)
+
+
---
+++ pacotes/funcoes.py
@@ -0,0 +1,11 @@
+
+def get_score(team_name : str, teams : list):
+    return f'A {team_name} ficou em {teams.index(team_name) + 1}'
+
+
+
+
+
+
+
+
